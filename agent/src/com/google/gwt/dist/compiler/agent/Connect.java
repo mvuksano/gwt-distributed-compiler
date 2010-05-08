045f424baa196d138dc8c3b1b76ce31516ed95b5
Marko Vuksanovic <markovuksanovic@gmail.com>
2010-05-08 13:42:50 +0200

    Refactoring.

--- agent/src/com/google/gwt/dist/compiler/agent/Connect.java
+++ agent/src/com/google/gwt/dist/compiler/agent/Connect.java
@@ -7,7 +7,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.Socket;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
