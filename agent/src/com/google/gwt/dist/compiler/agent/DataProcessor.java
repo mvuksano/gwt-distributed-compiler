a1c82ce4b21de106cb35a9c0d1ae58c07142736e
Marko Vuksanovic <markovuksanovic@gmail.com>
2010-05-08 19:04:18 +0200

    Added DataProcessor skeleton.

---
+++ agent/src/com/google/gwt/dist/compiler/agent/DataProcessor.java
@@ -0,0 +1,8 @@
+package com.google.gwt.dist.compiler.agent;
+
+/**
+ * Definition of objects that will handle data processing.
+ */
+public interface DataProcessor {
+
+}
--- core/src/com/google/gwt/dist/util/ZipDecompressor.java
+++ core/src/com/google/gwt/dist/util/ZipDecompressor.java
@@ -8,8 +8,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.zip.Adler32;
 import java.util.zip.CheckedInputStream;
 import java.util.zip.ZipEntry;
@@ -21,8 +19,6 @@ import java.util.zip.ZipInputStream;
  */
 public class ZipDecompressor {
 
-	private Logger logger = Logger.getLogger(ZipDecompressor.class.getName());
-
 	private static final int BUFFER = 2048;
 
 	/**
@@ -32,43 +28,40 @@ public class ZipDecompressor {
 	 *            ByteArrayOutputStream to be uncompressed.
 	 * @param directory
 	 *            The directory into which to store the uncompressed stream.
+	 * @throws IOException
+	 * @throws FileNotFoundException
 	 */
 	public void decompressAndStoreToFile(ByteArrayOutputStream stream,
-			File directory) {
-		try {
-			directory.mkdir();
-			BufferedOutputStream dest = null;
-			ByteArrayInputStream inputStream = new ByteArrayInputStream(stream.toByteArray());
-			CheckedInputStream checksum = new CheckedInputStream(inputStream,
-					new Adler32());
-			ZipInputStream zis = new ZipInputStream(new BufferedInputStream(
-					checksum));
-			ZipEntry entry;
-			while ((entry = zis.getNextEntry()) != null) {
-				
-				// Restore directory structure.
-				File destFile = new File(directory, entry.getName());
-				destFile = new File(destFile.getParent());
-				destFile.mkdirs();
+			File directory) throws FileNotFoundException, IOException {
+		directory.mkdir();
+		BufferedOutputStream dest = null;
+		ByteArrayInputStream inputStream = new ByteArrayInputStream(stream
+				.toByteArray());
+		CheckedInputStream checksum = new CheckedInputStream(inputStream,
+				new Adler32());
+		ZipInputStream zis = new ZipInputStream(new BufferedInputStream(
+				checksum));
+		ZipEntry entry;
+		while ((entry = zis.getNextEntry()) != null) {
 
-				int count;
-				byte data[] = new byte[BUFFER];
-				File f = new File(directory.getAbsolutePath() + Util.getFolderSeparatorInZipArchive() + entry.getName());
-				f.createNewFile();
-				FileOutputStream fos = new FileOutputStream(f);
-				dest = new BufferedOutputStream(fos, BUFFER);
-				while ((count = zis.read(data, 0, BUFFER)) != -1) {
-					dest.write(data, 0, count);
-				}
-				dest.flush();
-				dest.close();
+			// Restore directory structure.
+			File destFile = new File(directory, entry.getName());
+			destFile = new File(destFile.getParent());
+			destFile.mkdirs();
+
+			int count;
+			byte data[] = new byte[BUFFER];
+			File f = new File(directory.getAbsolutePath()
+					+ Util.getFolderSeparatorInZipArchive() + entry.getName());
+			f.createNewFile();
+			FileOutputStream fos = new FileOutputStream(f);
+			dest = new BufferedOutputStream(fos, BUFFER);
+			while ((count = zis.read(data, 0, BUFFER)) != -1) {
+				dest.write(data, 0, count);
 			}
-			zis.close();
-		} catch (FileNotFoundException e) {
-			logger.log(Level.SEVERE, "File not found!");
-		} catch (IOException e) {
-			logger.log(Level.SEVERE, "Problem writing to a file or directory.");
+			dest.flush();
+			dest.close();
 		}
-
+		zis.close();
 	}
 }
