ddf263d80a8382a8bcfa0f2a2957ff1351767f88
mv <markovuksanovic@gmail.com>
2010-05-13 13:29:49 +0200

    ZipComressor Test.

--- core/test/com/google/gwt/dist/ZipCompressorTest.java
+++ core/test/com/google/gwt/dist/ZipCompressorTest.java
@@ -1,32 +1,54 @@
 package com.google.gwt.dist;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.regex.Pattern;
+import java.util.zip.Adler32;
+import java.util.zip.CheckedInputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import com.google.gwt.dist.util.Util;
 import com.google.gwt.dist.util.ZipCompressor;
 
 public class ZipCompressorTest {
 
 	File dirToCompress;
 	ZipCompressor zipCompressor;
-	
+
 	@BeforeClass
 	public void setUp() {
-		dirToCompress = new File("resources\\sample\\");
+		File source = new File(System.getProperty("user.dir")).getParentFile();
+		dirToCompress = new File(
+				source.toString()
+						+ "\\core\\test\\com\\google\\gwt\\dist\\resources\\sample-to-be-compressed\\");
 		zipCompressor = new ZipCompressor();
 	}
-	
+
 	@Test
-	public void testArchiveAndCompress() {
-		try {
-			zipCompressor.archiveAndCompressDir(dirToCompress, Pattern.compile("permutation-[0-9+]*"));
-		} catch (IOException e) {
+	public void testArchiveAndCompress() throws IOException {
+		byte[] compressedOutput;
+		String[] expectedZipEntryNames = {
+				"permutation-0.js",
+				"test-folder" + Util.getFolderSeparatorInZipArchive()
+						+ "permutation-1.js" };
+		compressedOutput = zipCompressor.archiveAndCompressDir(dirToCompress,
+				Pattern.compile("permutation-[0-9+].js")).toByteArray();
+		ByteArrayInputStream is = new ByteArrayInputStream(compressedOutput);
+		CheckedInputStream checksum = new CheckedInputStream(is, new Adler32());
+		ZipInputStream zis = new ZipInputStream(new BufferedInputStream(
+				checksum));
+		ZipEntry zipEntry;
+		int counter = 0;
+		while ((zipEntry = zis.getNextEntry()) != null) {
+			Assert.assertEquals(zipEntry.getName(),
+					expectedZipEntryNames[counter++]);
 		}
-		Assert.assertTrue(true);
 	}
 }
