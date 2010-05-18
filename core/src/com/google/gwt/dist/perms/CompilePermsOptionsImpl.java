58a2c496b9c099acc6bfc484f054e2c008de319e
mv <markovuksanovic@gmail.com>
2010-05-18 21:13:35 +0200

    A few more changes.

---
+++ core/src/com/google/gwt/dist/perms/CompilePermsOptionsImpl.java
@@ -0,0 +1,45 @@
+package com.google.gwt.dist.perms;
+
+import com.google.gwt.dev.CompilerOptions;
+import com.google.gwt.dev.CompilePerms.CompilePermsOptions;
+import com.google.gwt.dist.compiler.impl.CompileTaskOptionsImpl;
+
+/**
+ * Concrete class to implement compiler perm options.
+ */
+public class CompilePermsOptionsImpl extends CompileTaskOptionsImpl implements
+    CompilePermsOptions {
+
+  private int localWorkers;
+  private int[] permsToCompile;
+
+  public CompilePermsOptionsImpl() {
+  }
+
+  public CompilePermsOptionsImpl(CompilerOptions other) {
+    copyFrom(other);
+  }
+
+  public void copyFrom(CompilePermsOptions other) {
+    super.copyFrom(other);
+    setPermsToCompile(other.getPermsToCompile());
+    setLocalWorkers(other.getLocalWorkers());
+  }
+
+  public int getLocalWorkers() {
+    return localWorkers;
+  }
+
+  public int[] getPermsToCompile() {
+    return (permsToCompile == null) ? null : permsToCompile.clone();
+  }
+
+  public void setLocalWorkers(int localWorkers) {
+    this.localWorkers = localWorkers;
+  }
+
+  public void setPermsToCompile(int[] permsToCompile) {
+    this.permsToCompile = (permsToCompile == null) ? null
+        : permsToCompile.clone();
+  }
+}
