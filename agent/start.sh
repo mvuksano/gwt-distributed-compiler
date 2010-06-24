054984d08350e5823e8abb6566c181d7a45083bc
markovuksanovic <markovuksanovic@gmail.com>
2010-06-24 17:13:44 +0200

    minor changes

--- agent/start.sh
+++ agent/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-java -Xmx1024m -Xms1024m -classpath /home/markovuksanovic/devel/open-source/gwt-distributed-compiler/agent/dist/*:lib/*:/home/markovuksanovic/devel/gwt/trunk/build/staging/gwt-0.0.0/* com.google.gwt.dist.compiler.agent.Agent
+java -agentpath:/home/markovuksanovic/bin/yjp-9.0.5/bin/linux-x86-64/libyjpagent.so=onexit=snapshot -Xmx1024m -Xms1024m -classpath /home/markovuksanovic/devel/open-source/gwt-distributed-compiler/agent/dist/*:lib/*:/home/markovuksanovic/devel/gwt/trunk/build/staging/gwt-0.0.0/* com.google.gwt.dist.compiler.agent.Agent
 
